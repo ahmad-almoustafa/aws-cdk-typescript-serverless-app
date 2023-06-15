e309607d37194335793c2a49e540006bea29e861
Ahmad Al Moustafa <almoustafa.ahmad@gmail.com>
2023-06-15 17:52:12 +0800

    CDK constructs levels

--- lib/aws-cdk-demo-stack.ts
+++ lib/aws-cdk-demo-stack.ts
@@ -1,16 +1,63 @@
 import * as cdk from 'aws-cdk-lib';
+import { Duration } from 'aws-cdk-lib';
+import { CfnBackupPlan } from 'aws-cdk-lib/aws-backup';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+//if you want to use a construct beyond the Level 1 and Level 2 => you need to define and implement your own custom Level 3 construct 
+export class customL3Bucket extends Construct {
+  constructor(scope: Construct, id: string, expirationInDays: number) {
+    super(scope, id);
+    new CfnBucket(this, id, {
+      bucketName: id.toLowerCase(),//Bucket name should not contain uppercase characters
+      lifecycleConfiguration: {
+        rules: [{
+          expirationInDays: expirationInDays,
+          status: 'Enabled',
+        }]
+      }
+    });
+  }
+
+}
 export class AwsCdkDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    /**
+     * level 1 construct 
+     * lower level => more flexibility and control
+     * directly represents the AWS CloudFormation resource type AWS::S3::Bucket => one-to-one mapping
+     * explicitly define all the properties and their corresponding CloudFormation attributes.
+     */
+    new CfnBucket(this, 'AWS-CDK-L1-Bucket', {
+      bucketName: 'aws-cdk-l1-bucket',
+      lifecycleConfiguration: {
+        rules: [{
+          expirationInDays: 1,
+          status: 'Enabled',
+        }]
+      }
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    /**
+     * level 2 construct 
+     * higher level => more  abstracted and developer-friendly 
+     * It abstracts away many of the low-level details and provides simplified methods and properties for common bucket configurations.
+     * used for most S3 bucket scenarios
+     */
+    const l2Bucket=new Bucket(this, 'AWS-CDK-L2-Bucket', {
+      bucketName: 'aws-cdk-l2-bucket',
+      lifecycleRules: [{
+        expiration: Duration.days(3),
+      }]
+    })
+
+    //level 3 construct => define your own
+    const l3Bucket = new customL3Bucket(this, 'AWS-CDK-L3-Bucket', 1);
+
   }
+
+
 }
