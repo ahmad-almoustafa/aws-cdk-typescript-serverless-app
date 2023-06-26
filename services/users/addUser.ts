5f968add76ec807faf24ee355c986047fb377129
Ahmad Al Moustafa <almoustafa.ahmad@gmail.com>
2023-06-26 17:41:55 +0800

    DynamoDB: marshaling

--- services/users/addUser.ts
+++ services/users/addUser.ts
@@ -1,22 +1,31 @@
 
-import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { AttributeValue, DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { marshall } from "@aws-sdk/util-dynamodb";
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { v4 } from "uuid";
 export const addUser=async (event:APIGatewayProxyEvent, dynamoDBClient:DynamoDBClient):Promise<APIGatewayProxyResult>=>{
     const randomId=v4();
     if( event.body && process.env.dynamoDBTable ){
-        const Item=JSON.parse(event.body);
+        const item=JSON.parse(event.body);
+        item.id=randomId;
+        
+        /**
+         * Convert the item to DynamoDB attribute values
+         * Record<K, T>: Typescript utility type =>
+         * Creates an object type where the keys are of type K and the values are of type T.
+         * {S:item.name}=><K,T> 
+         * */ 
+        let marshalledItem: Record<string, AttributeValue> = {};
+        Object.entries(item).forEach(([key, value]) => {
+            marshalledItem[key] = marshall(value);
+          });
         const params={
             TableName: process.env.dynamoDBTable,        
             /**
              * SDK 3 and typescript you need to specify the data types of attribute values.
              * by default the created DynamoDB table would have Partition key(id:String) => Item must include id
              */
-            Item:{
-                id:{S:randomId},
-                firstName: {S:Item.firstName},
-                lastName: {S:Item.lastName},
-            }
+            Item:marshalledItem,
         }
 
         const command= new PutItemCommand(params);
