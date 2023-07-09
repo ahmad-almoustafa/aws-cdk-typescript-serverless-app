c6ae758885ade5554c466c6f14fdcfc0aabd0b2b
Ahmad Al Moustafa <almoustafa.ahmad@gmail.com>
2023-07-09 19:38:27 +0800

    Frontend: authenticat React app users using Cognito

--- frontend/src/services/AuthService.ts
+++ frontend/src/services/AuthService.ts
@@ -1,10 +1,28 @@
-export class AuthService{
-    public async login(username: string, password: string): Promise<object| undefined>{
-        return {
-            user:'Test User',
-        }
+import { Amplify,Auth } from 'aws-amplify';
+import {AuthStack} from './../../../backend/outputs.json'; // import from cloud formation exported outputs
+import { type CognitoUser } from '@aws-amplify/auth';
+
+
+
+const awsRegion='ap-southeast-2';
+Amplify.configure({
+    Auth: {
+        region: awsRegion,
+        userPoolId: AuthStack.ProductUserPoolId,
+        identityPoolId: AuthStack.ProductIdentityPoolId,
+        userPoolWebClientId: AuthStack.ProductUserPoolClientId,
+        authenticationFlowType: 'USER_PASSWORD_AUTH'
+
     }
-    public  getProfileName():string{
-        return 'Test User'
+});  
+export class AuthService{
+    private user: CognitoUser | undefined;
+    //login('test_user_name','Mki9!1wY2Hn');
+    public async login(username: string, password: string): Promise<object| undefined>{
+        this.user= await Auth.signIn(username, password) as CognitoUser;
+        return  this.user;
+    }
+    public  getProfileName():string|undefined{
+        return this.user?.getUsername();
     }
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // fix: global is not defined
+    global: {},
+  },
 
 })
